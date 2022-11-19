f5ce80c4d03ae00b043763b3b0d1470c26213f04
shungo0222 <kimura.shungo@gmail.com>
2022-11-20 02:51:58 +0900

    make styles dir and move newMint.tsx to pages dir

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,20 @@
-import type { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
-import WalletContextProvider from '../components/WalletContextProvider'
+import type { AppProps } from "next/app"
+import { ChakraProvider, extendTheme } from "@chakra-ui/react"
+import WalletContextProvider from "../components/WalletContextProvider"
+
+const colors = {
+  background: "#1F1F1F",
+  accent: "#833BBE",
+  bodyText: "rgba(255, 255, 255, 0.75)",
+}
+
+const theme = extendTheme({ colors })
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <WalletContextProvider>
-        <Component {...pageProps}/>
+        <Component {...pageProps} />
       </WalletContextProvider>
     </ChakraProvider>
   )
